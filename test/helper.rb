26e1340a1bfdbadd6ea9a2819ea7300e006982c4
mcnelson <michael@nelsonware.com>
2013-11-10 21:48:12 -0800

    Move casettes to a better spot

--- test/helper.rb
+++ test/helper.rb
@@ -4,7 +4,7 @@ require "vcr"
 require "webmock"
 
 VCR.configure do |c|
-  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.cassette_library_dir = 'test/cassettes'
   c.hook_into :webmock
 end
 
